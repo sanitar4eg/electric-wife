3749242c268f4657303eaac3f6e6dad434e73218
Tsvetkovv <11594423+Tsvetkovv@users.noreply.github.com>
2018-03-24 20:02:40 +0400

    added mirroring

--- app/src/main/java/org/edu/electricwife/app/SpeechToTextActivity.java
+++ app/src/main/java/org/edu/electricwife/app/SpeechToTextActivity.java
@@ -3,19 +3,26 @@ package org.edu.electricwife.app;
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.UtteranceProgressListener;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.security.KeyPairGenerator;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Locale;
 
 public class SpeechToTextActivity extends Activity {
     private TextView txtSpeechInput;
     private Button btnSpeak;
+    private TextToSpeech tts;
     private final int REQ_CODE_SPEECH_INPUT = 100;
 
     @Override
@@ -30,18 +37,33 @@ public class SpeechToTextActivity extends Activity {
                 promptSpeechInput();
             }
         });
+        tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    int result = tts.setLanguage(Locale.US);
+                    if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                        Log.e("TTS", "This Language is not supported");
+                    }
+                    speak("Hello");
+
+                } else {
+                    Log.e("TTS", "Initilization Failed!");
+                }
+            }
+        });
     }
 
     /**
      * Showing google speech input dialog
-     * */
+     */
     private void promptSpeechInput() {
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
                 RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
         intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
-               "Say something");
+                "Say something");
         try {
             startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
         } catch (ActivityNotFoundException a) {
@@ -51,9 +73,34 @@ public class SpeechToTextActivity extends Activity {
         }
     }
 
+    private void speak(String text) {
+        if (text != null) {
+            HashMap<String, String> hashTts = new HashMap<>();
+            hashTts.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, "id");
+
+            tts.speak(text, TextToSpeech.QUEUE_FLUSH, hashTts);
+            tts.setOnUtteranceProgressListener(mProgressListener);
+        }
+    }
+    private UtteranceProgressListener mProgressListener = new UtteranceProgressListener() {
+        @Override
+        public void onStart(String utteranceId) {
+        } // Do nothing
+
+        @Override
+        public void onError(String utteranceId) {
+        } // Do nothing.
+
+        @Override
+        public void onDone(String utteranceId) {
+            promptSpeechInput();
+        }
+    };
+
+
     /**
      * Receiving speech input
-     * */
+     */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -65,10 +112,20 @@ public class SpeechToTextActivity extends Activity {
                     ArrayList<String> result = data
                             .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
                     this.txtSpeechInput.setText(result.get(0));
+                    speak((String) txtSpeechInput.getText());
                 }
                 break;
             }
 
         }
+    }
+
+    @Override
+    public void onDestroy() {
+        if (tts != null) {
+            tts.stop();
+            tts.shutdown();
+        }
+        super.onDestroy();
     }
 }
