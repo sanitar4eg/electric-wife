5f0e2c4f5e21ed9565a77f3d99394d61e9aa166e
Tsvetkovv <11594423+Tsvetkovv@users.noreply.github.com>
2018-03-24 13:39:50 +0400

    added speech screen

---
+++ app/src/main/java/org/edu/electricwife/app/SpeechToTextActivity.java
@@ -0,0 +1,13 @@
+package org.edu.electricwife.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SpeechToTextActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_speech_to_text);
+    }
+}
