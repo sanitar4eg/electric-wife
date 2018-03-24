5045b43ee82772328895a16434c9c6d95d19fe80
Denis_Mezhuev <denis_mezhuev@epam.com>
2018-03-24 20:16:31 +0400

    Add people support

--- app/src/main/java/org/edu/electricwife/app/MainActivity.java
+++ app/src/main/java/org/edu/electricwife/app/MainActivity.java
@@ -1,24 +1,46 @@
 package org.edu.electricwife.app;
 
+import android.accounts.Account;
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 
 import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.tasks.Task;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.people.v1.PeopleService;
+import com.google.api.services.people.v1.model.ListConnectionsResponse;
+import com.google.api.services.people.v1.model.Person;
+
+import java.lang.ref.WeakReference;
+import java.util.Collections;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener {
 
     private static final int RC_AUTH_CODE = 111;
     private static final String TAG = "GoogleOAuthClient";
+    private static final String CONTACTS_SCOPE = "https://www.googleapis.com/auth/contacts.readonly";
+    private static final HttpTransport HTTP_TRANSPORT = AndroidHttp.newCompatibleTransport();
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+
     /* Client for accessing Google APIs */
     private static GoogleApiClient mGoogleApiClient;
 
@@ -30,7 +52,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestServerAuthCode(getString(R.string.server_client_id))
                 .requestEmail()
-                .requestScopes(new Scope("https://www.googleapis.com/auth/youtube.readonly"))
+                .requestScopes(
+                        new Scope("https://www.googleapis.com/auth/youtube.readonly"),
+                        new Scope(CONTACTS_SCOPE)
+                )
                 .build();
 
         mGoogleApiClient = new GoogleApiClient.Builder(this)
@@ -74,14 +99,108 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == RC_AUTH_CODE) {
-            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            if (result.isSuccess()) {
-                GoogleSignInAccount acct = result.getSignInAccount();
-                if (acct != null) {
-                    Log.i(TAG, acct.getEmail());
-                    String authCode = acct.getServerAuthCode();
-//                getAccessToken(authCode);
+
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> task) {
+        try {
+            GoogleSignInAccount account = task.getResult(ApiException.class);
+
+            String serverAuthCode = account.getServerAuthCode();
+
+            Log.i(TAG, "serverAuthCode=" + serverAuthCode);
+            updateUI(account);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+            updateUI(null);
+        }
+    }
+
+    private void updateUI(@Nullable GoogleSignInAccount account) {
+        if (account != null) {
+            new GetContactsTask(this).execute(account.getAccount());
+        } else {
+
+        }
+    }
+
+    protected void onConnectionsLoadFinished(@Nullable List<Person> connections) {
+        if (connections == null) {
+            Log.i(TAG, "getContacts:connections: null");
+            return;
+        }
+
+        Log.d(TAG, "getContacts:connections: size=" + connections.size());
+
+        // Get names of all connections
+        StringBuilder msg = new StringBuilder();
+        for (int i = 0; i < connections.size(); i++) {
+            Person person = connections.get(i);
+            if (person.getNames() != null && person.getNames().size() > 0) {
+                msg.append(person.getNames().get(0).getDisplayName());
+
+                if (i < connections.size() - 1) {
+                    msg.append(",");
                 }
+            }
+        }
+
+        // Display names
+        Log.i(TAG, msg.toString());
+    }
+
+
+    private static class GetContactsTask extends AsyncTask<Account, Void, List<Person>> {
+
+        private WeakReference<MainActivity> mActivityRef;
+
+        public GetContactsTask(MainActivity activity) {
+            mActivityRef = new WeakReference<>(activity);
+        }
+
+        @Override
+        protected List<Person> doInBackground(Account... accounts) {
+            if (mActivityRef.get() == null) {
+                return null;
+            }
+
+            Context context = mActivityRef.get().getApplicationContext();
+            try {
+                GoogleAccountCredential credential = GoogleAccountCredential.usingOAuth2(
+                        context,
+                        Collections.singleton(CONTACTS_SCOPE));
+                credential.setSelectedAccount(accounts[0]);
+
+                PeopleService service = new PeopleService.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
+                        .setApplicationName("Google Sign In Quickstart")
+                        .build();
+
+                ListConnectionsResponse connectionsResponse = service
+                        .people()
+                        .connections()
+                        .list("people/me")
+                        .setPersonFields("names,emailAddresses")
+                        .execute();
+
+                return connectionsResponse.getConnections();
+
+            } catch (Exception e) {
+                Log.w(TAG, "getContacts:exception", e);
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(List<Person> people) {
+            super.onPostExecute(people);
+            if (mActivityRef.get() != null) {
+                mActivityRef.get().onConnectionsLoadFinished(people);
             }
         }
     }
